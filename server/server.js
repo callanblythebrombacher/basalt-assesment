c1029d0009cc506a36b4a8575d02ae9ce7441ff0
callanblythebrombacher <callan@sitebydesign.co.za>
2023-03-31 23:56:52 +0200

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+import ServerlessHttp from "serverless-http";
+import express from 'express';
+
+const app = express()
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+module.exports.handler = ServerlessHttp(app);
